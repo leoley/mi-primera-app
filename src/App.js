9f511a0b69efe2d52b3ff97ca37e3d203f89497a
Lionard Leyva <lionard.leyva@rappi.com>
2022-09-04 01:03:57 -0500

    feat: Props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
+import HolaMundo from './components/HolaMundo';
+import AdiosMundo from './components/AdiosMundo';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <HolaMundo></HolaMundo>
+        <AdiosMundo></AdiosMundo>
+        <p>Lionard Leyva</p>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/HolaMundo.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function HolaMundo(){
+    const miFunction = () => {
+        console.log("mi funcion");
+    }
+    return(
+        <>
+            <h1>Hola Mundo</h1>
+            
+        </>
+    )
+}

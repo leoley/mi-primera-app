551a5df6793e8f4f58a39ce10f8180f99f07d755
Lionard Leyva <lionard.leyva@rappi.com>
2022-09-08 00:04:27 -0500

    USESTATE import React, { useState } from 'react';

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React, { useState } from 'react';
 import SaludoButton from './components/SaludoButton';
 
 export default function App() {
-
+  //stateCar => valor del estado
+  //setStateCar => Hago un set del valor
+  const [stateCar, setStateCar] = useState(false)
 
   const user = {
     nombre: "",
@@ -12,11 +14,17 @@ export default function App() {
     color: "azul"
   }
 
-
   const saludarFN = (name, edad) => {
     console.log("Hola " + name + "tiene" + edad + "años")
     console.log(`Hola ${name}, tiene ${edad}`)
   };
+
+  const encenderApagar = () => {
+    setStateCar(!stateCar)
+  //  setStateCar(prevValue => !prevValue)
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -27,6 +35,8 @@ export default function App() {
           saludarFn={saludarFN}>
         </SaludoButton>
 
+        <h3>El coche esta: {stateCar ? "Encendido" : "Apagado"}</h3>
+        <button onClick={encenderApagar}>Encender</button>
       </header>
     </div>
   );
